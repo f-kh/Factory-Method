26b4147691796f64ccf8deba79c831d377190b4d
f-kh <62787949+f-kh@users.noreply.github.com>
2020-05-25 16:31:47 +0430

    Update PlayTest.php

--- tests/Unit/Play/PlayTest.php
+++ tests/Unit/Play/PlayTest.php
@@ -1,1 +1,23 @@
 
+<?php
+
+
+namespace Tests\Unit\Play;
+
+use App\Play\PlayFactory;
+use Tests\TestCase;
+
+class PlayTest extends TestCase
+{
+    public function test_play()
+    {
+        $players = ['goalkeeper', 'defender', 'midfielder', 'forward', 'striker'];
+        $playerIndex = array_rand($players);
+
+        ((new PlayFactory())->setPost($players[$playerIndex]))->run();
+
+        $this->assertTrue(true);
+
+    }
+
+}
