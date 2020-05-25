8c8a801921b0b7fd667ac5400c7ec0adf451f05d
f-kh <62787949+f-kh@users.noreply.github.com>
2020-05-25 16:30:27 +0430

    Update LogTest.php

--- tests/Unit/Log/LogTest.php
+++ tests/Unit/Log/LogTest.php
@@ -1,1 +1,20 @@
 
+<?php
+
+namespace Tests\Unit\Log;
+
+use App\Log\HandleLog;
+use Tests\TestCase;
+
+class LogTest extends TestCase
+{
+    public function test_print_log()
+    {
+        $message = new HandleLog();
+        $message->printLog('error');
+
+        var_dump($message->getMessage());
+
+        $this->assertTrue(true);
+    }
+}
